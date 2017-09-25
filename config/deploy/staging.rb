40a93ff9ff4fe961b058e38d57b250267e1cc0d2
Kevin Ulrich Moschallski <kevin@digitalpatrioten.com>
2017-09-25 11:32:03 +0200

    Adjusted deployement

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,6 @@ set :deploy_to, '/var/www/web.typo3.project.kandidat-o-mat'
 # if you haven't already specified
 set :rails_env, "staging"
 SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
-set :composer_working_dir, -> { File.join(fetch(:release_path), 'htdocs') }
 
 namespace :deploy do
     desc 'Clear cache'
